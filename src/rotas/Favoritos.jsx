3c5973ca009ee35c6f23f3fdb13f95a784e90cf2
Dirceusljr <dirceusljr@gmail.com>
2024-08-08 16:36:36 -0300

    feat: :sparkles: add: rota favoritos

---
+++ src/rotas/Favoritos.jsx
@@ -0,0 +1,13 @@
+import Pesquisa from "../components/Pesquisa";
+import UltimosLancamentos from "../components/UltimosLancamentos";
+
+function Favoritos() {
+  return (
+    <div className=" w-screen h-screen bg-gradiente-cresc ">
+      <Pesquisa />
+      
+    </div>
+  );
+}
+
+export default Favoritos;
